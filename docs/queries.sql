a41aa44f0848a35377fedd3bc880288a115ac21a
suphotsudsee <suphotsudsee@gmail.com>
2026-03-22 16:41:32 +0700

    ll

--- docs/queries.sql
+++
@@ -1,9 +0,0 @@
--- QUERY: PERSON
-SELECT
-    '{hcode}' AS hcode,
-    '{date}' AS report_date,
-    DATE_FORMAT('{date}', '%Y-%m') AS report_period,
-    COUNT(*) AS total_person,
-    SUM(CASE WHEN sex = '1' THEN 1 ELSE 0 END) AS male,
-    SUM(CASE WHEN sex = '2' THEN 1 ELSE 0 END) AS female
-FROM person;
